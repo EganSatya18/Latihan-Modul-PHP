436cc26df6eaeb908871aa0c7a357f5f670a3e79
Egan Satya Pricilio <87957664+EganSatya18@users.noreply.github.com>
2021-09-08 08:15:38 +0700

    Delete Modul 3 directory

--- Modul 3/Latihan 3.3.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $user = "Firdausa";
-    $pass = "123";
-    if ($user == "firdausa" && $pass == "123"){
-        echo "Login Berhasil";
-    } else {
-        echo "Login Gagal";
-    }
-?>
